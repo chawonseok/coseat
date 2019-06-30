95f87509027b8fdfba39934c7889ada221baea46
Chacrew <Chacrew@DESKTOP-C2CTIJ2>
2019-06-30 22:45:53 +0900

    왜 안뜨지...개빡...2019/06/30 10:45 차원석

--- src/main/java/com/web/controller/exampleController.java
+++ src/main/java/com/web/controller/exampleController.java
@@ -1,9 +1,31 @@
 package com.web.controller;
 
-@Autowired
-private ReviewDao reviewDao;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.web.dao.ExampleDao;
+import com.web.dto.Example;
+
+
+@Controller
+@RequestMapping("/example/*")
 public class exampleController {
+	@Autowired
+	private ExampleDao exampleDao;
 	
+	@RequestMapping(value="example",method=RequestMethod.GET )
+	private String example(String pk_num,String name, int age,Model model) {
+		List<Example> exam;
+		exam = exampleDao.get();
+		model.addAttribute("exam", exam);
+		
+		return "example.example";
+		// TODO Auto-generated method stub
 
+	} 
 }
