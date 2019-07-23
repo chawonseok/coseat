6a27054350fca6338606e2bbdb4a7a5b9e28d22d
LeoHanSung <5001jhs@gmail.com>
2019-07-23 23:02:32 +0900

    20190723 정한성

---
+++ src/main/java/com/web/controller/HsController.java
@@ -0,0 +1,23 @@
+package com.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+@RequestMapping("/*")
+public class HsController {
+	
+	@RequestMapping(value="hs",method=RequestMethod.GET )
+	private String example() {
+		System.out.println("³ª¿Í¶ù");
+		return "template";
+		// TODO Auto-generated method stub
+
+	} 
+}
